468a1b10b8883a878308cca3c8ff9ada46fe74cc
Ravi kumar <ravirajx2012@gmail.com>
2020-06-21 00:19:35 +0530

    Permutation Sequence

---
+++ Solution4.java
@@ -0,0 +1,37 @@
+class Solution4 {
+     
+    
+
+    public String swap(String a, int i, int j) 
+    { 
+        char temp; 
+        char[] charArray = a.toCharArray(); 
+        temp = charArray[i]; 
+        charArray[i] = charArray[j]; 
+        charArray[j] = temp; 
+        return String.valueOf(charArray); 
+    } 
+    public String getPermutation(int n, int k) {
+    int[] factorial = new int[n];
+    int i;
+    StringBuilder sb = new StringBuilder();
+    ArrayList<Integer> num = new ArrayList<Integer>();
+
+    for (i = 0; i < n; i++) {
+        num.add(i + 1);
+        factorial[i] = i == 0 ? 1 : i * factorial[i - 1];
+    }
+
+    while (n > 0) {
+        int remaining = ((k - 1) % factorial[n - 1]) + 1;
+        sb.append(num.remove(((k - 1) / factorial[n - 1])));
+        n--;
+        k = remaining;
+    }
+
+    return sb.toString();
+
+        
+        
+    }
+}
